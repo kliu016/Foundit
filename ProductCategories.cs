6e8d946b9b84cd77db558868c33fa98648fa183d
Kevin Liu <kliu016@gmail.com>
2017-02-22 13:20:50 -0800

    ProductCategories Domain

---
+++ ProductCategories.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sabio.Web.Domain
+{
+    public class ProductCategory
+    {
+        public int ParentCategoryId { get; set; }
+
+        public string DisplayName { get; set; }
+
+        public string ProductCategoryKey { get; set; }
+
+        public string UserId { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public int Id { get; set; }
+
+        public bool IsDeleted { get; set; }
+
+        public DateTime DateCreated { get; set; }
+
+        public DateTime DateModified { get; set; }
+
+
+    }
+}
---
+++ ProductCategoryKeywords.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+namespace Sabio.Web.Domain
+{
+    public class ProductCategoryKeyword
+    {
+        public int Id { get; set; }
+
+        public DateTime DateCreated { get; set; }
+        public DateTime DateModified { get; set; }
+        public string Keyword { get; set; }
+        public int CategoryId { get; set; }
+    }
+}
