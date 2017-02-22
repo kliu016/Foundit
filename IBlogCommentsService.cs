969d355799749291981ce9f457dfb460d54e6a78
Kevin Liu <kliu016@gmail.com>
2017-02-22 12:31:31 -0800

    Blog Comments and Posts Interfaces

---
+++ IBlogCommentsService.cs
@@ -0,0 +1,24 @@
+﻿using Sabio.Web.Domain;
+using Sabio.Web.Models.Requests;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sabio.Web.Services.Interfaces
+{
+   public interface IBlogCommentsService
+    {
+       int CreateBlog(BlogCommentsCreateRequest newBlog);
+
+       void UpdateBlog(BlogCommentsUpdateRequest model);
+
+        void DeleteComment(int Id);
+
+        BlogComment GetById(int Id);
+
+        List<BlogComment> GetAll();
+
+    }
+}
---
+++ IBlogPostsService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sabio.Web.Domain;
+using Sabio.Web.Models.Requests;
+
+namespace Sabio.Web.Services.Interfaces
+{
+    public interface IBlogPostsService
+    {
+        int CreateBlog(BlogPostCreateRequest model);
+        void UpdateBlog(BlogPostUpdateRequest model);
+        //List<BlogPost> SelectAll();
+        void Delete(int id);
+        void Restore(int Id);
+        BlogPost GetById(int id);
+        BlogPost GetByDateAndTitle(int year, int month, int day, string title);
+        List<MetaTag> GetMetaTags(int year, int month, int day, string title);
+        PagedList<BlogPost> GetTop(int pageIndex, int itemsPerPage);
+        PagedList<BlogPost> GetConsumer(int pageIndex, int numberOfRecords);
+        PagedList<BlogPost> GetAdmin(int pageIndex, int numberOfRecords);
+        PagedList<BlogPost> GetByTag(string blogTag, int pageIndex, int itemsPerPage);
+    }
+}
